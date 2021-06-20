372bdf0f76a6d2102fb25d9edba1428334fbd5d3
rajoriaakash <akashrajoria250103@gmail.com>
2021-06-20 12:03:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/callfaker/FakeRingingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.callfaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FakeRingingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_ringing)
+    }
+}
